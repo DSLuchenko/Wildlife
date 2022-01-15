32643a6bbd34ec0b8d9033b127cc124b5dcfa280
DSLuchenko <DSLUchenko@bk.ru>
2022-01-15 23:00:03 +0300

    swiper

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const swiper = new Swiper(".articles-block__slider", {
+const swiper = new Swiper(".swiper", {
     slidesPerView: 3,
     spaceBetween: 60,
     slidesPerGroup: 1,
-    centeredSlides: true,
-    loop: true,
-    loopFillGroupWithBlank: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        665: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        },
+         1200: {
+            spaceBetween: 60
+        }
     }
 });
